2221c3ccc59ab2225dc9298a2257ec8c72bf9873
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-08 10:17:41 -0300

    add secret kay to .env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@qcr3q#3r)q9&n5o!-!cxex3i+46#rom^(r=$d1zo*^yh)l=co'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['PRODUCTION']
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
