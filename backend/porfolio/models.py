ba6fa09f1d743a6203409cf3a088493cc6b9672b
Manuel Lopez Jove <liaimasoft@gmail.com>
2024-03-31 14:44:03 -0300

    skils models

--- backend/porfolio/models.py
+++ backend/porfolio/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.db.models.fields import CharField, URLField
+from django.db.models.fields import CharField, URLField, IntegerField, TextField
 from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
-    description = CharField(max_length=250)
+    description = TextField(max_length=250)
     image = ImageField(upload_to='portfolio/images/')
     url = URLField(blank=True)
+    position = IntegerField(default = 0)
+
+class Skills(models.Model):
+    name = CharField(max_length=100)
+    value = IntegerField(default = 0)
