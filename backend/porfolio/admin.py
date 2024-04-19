d423c3dd76c5faab82ae0470e939563e81e3d5de
Manuel Lopez Jove <liaimasoft@gmail.com>
2024-04-19 12:21:10 -0300

    fix

--- backend/porfolio/admin.py
+++ backend/porfolio/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Project, Skill, Work
 
-admin.site.register(Project, Skill, Work)
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Work)
